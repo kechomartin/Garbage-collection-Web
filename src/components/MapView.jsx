c5e94fc9219506389e27c640614ac139f8e52f18
mk0 <kecho.mk@gmail.com>
2025-06-12 19:39:00 +0300

    TEST MODE

---
+++ src/components/MapView.jsx
@@ -0,0 +1,11 @@
+const MapView = () => {
+  console.log("✅ MapView.jsx is rendering");
+
+  return (
+    <div className="bg-yellow-200 h-40 w-full text-center p-4 rounded-lg">
+      📍 MapView component is rendering!
+    </div>
+  );
+};
+
+export default MapView;
